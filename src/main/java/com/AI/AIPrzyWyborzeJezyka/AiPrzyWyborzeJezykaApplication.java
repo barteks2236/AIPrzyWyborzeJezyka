9ab69636251964d60134a58b8e47a65e3acb786b
barteks2236 <barteks2236@wp.pl>
2020-08-01 20:24:09 +0200

    Poczatek

---
+++ src/main/java/com/AI/AIPrzyWyborzeJezyka/AiPrzyWyborzeJezykaApplication.java
@@ -0,0 +1,13 @@
+package com.AI.AIPrzyWyborzeJezyka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPrzyWyborzeJezykaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPrzyWyborzeJezykaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AI/AIPrzyWyborzeJezyka/AiPrzyWyborzeJezykaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI.AIPrzyWyborzeJezyka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPrzyWyborzeJezykaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
